3e7f2c42b90dcc98428c7cf633c870054877d16b
naolgithub <naolcovered@gmail.com>
2024-02-17 11:52:40 +0300

    Cross fitness

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  /*
+  SystemChrome is there to make sure the
+  entire UI won’t turn around when the
+  user rotates their phone 90 degrees.
+  */
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+    return MaterialApp(
+      title: 'Cross Fitness',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: ColorConstants.textColor),
+        ),
+        fontFamily: 'NotoSansKR',
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: isLoggedIn ? TabBarPage() : OnboardingPage(),
+    );
+  }
+}
