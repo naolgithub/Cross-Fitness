b68a08f7aef06bb296c6243bdb99cfe34573fa93
naolgithub <naolcovered@gmail.com>
2024-02-19 12:02:18 +0300

    onboarding page is added

---
+++ lib/screens/onboarding/page/onboarding_page.dart
@@ -0,0 +1,38 @@
+import 'package:cross_fitness/screens/onboarding/bloc/onboarding_bloc.dart';
+import 'package:cross_fitness/screens/onboarding/widget/onboarding_content.dart';
+import 'package:cross_fitness/screens/sign_up/page/sign_up_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(context),
+    );
+  }
+
+  BlocProvider<OnboardingBloc> _buildBody(BuildContext context) {
+    return BlocProvider<OnboardingBloc>(
+      create: (BuildContext context) => OnboardingBloc(),
+      child: BlocConsumer<OnboardingBloc, OnboardingState>(
+        listenWhen: (_, currState) => currState is NextScreenState,
+        listener: (context, state) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (_) {
+                return const SignUpPage();
+              },
+            ),
+          );
+        },
+        buildWhen: (_, currState) => currState is OnboardingInitial,
+        builder: (context, state) {
+          return const OnboardingContent();
+        },
+      ),
+    );
+  }
+}
