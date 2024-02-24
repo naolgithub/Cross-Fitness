915bc80654468b5a843168bbf4428fee78656620
naolgithub <naolcovered@gmail.com>
2024-02-24 16:20:17 +0300

    onboarding modified

--- lib/screens/onboarding/bloc/onboarding_bloc.dart
+++ lib/screens/onboarding/bloc/onboarding_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
 part 'onboarding_event.dart';
