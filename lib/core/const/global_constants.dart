4094433f019384af5fca45b74421cf73b76b2384
naolgithub <naolcovered@gmail.com>
2024-02-24 16:15:53 +0300

    global constants added

---
+++ lib/core/const/global_constants.dart
@@ -0,0 +1,7 @@
+import 'package:cross_fitness/data/another_workout_data.dart';
+import 'package:cross_fitness/data/user_data.dart';
+
+class GlobalConstants {
+  static late UserData currentUser;
+  static late List<WorkoutData> workouts;
+}
